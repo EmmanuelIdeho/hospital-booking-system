b468f2230fceaa50b1ce5ab812acc6ccfb40ea0a
EmmanuelIdeho <ohis98@gmail.com>
2025-05-26 13:49:03 -0400

    frontend: Created Navbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 //import { useState } from 'react'
 import BookingForm from './components/BookingForm'
+import Navbar from './components/Navbar'
 import './App.css'
 //import axios from 'axios'
 
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+    <Navbar />
       <BookingForm />
     </>
     
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,21 @@
+import { AppBar, Toolbar, Typography, Button, Stack } from "@mui/material";
+
 function Navbar() {
 
+  const pages = ['Modify', 'Book', 'Appointments']
+
     return (
-      <>
-      </>
+      <AppBar position="fixed">
+        <Toolbar sx={{ justifyContent: 'center'}}>
+          <Stack direction={'row'} spacing={5}>
+          {pages.map((page) => (
+                <Button key={page}>
+                  <Typography sx={{ textAlign: 'center', paddingX: 2}}>{page}</Typography>
+                </Button>
+            ))}
+          </Stack>
+        </Toolbar>
+      </AppBar>
       
     )
   }
