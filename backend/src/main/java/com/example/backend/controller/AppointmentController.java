230b8394712c20a706ca011e5f0b68b195264166
EmmanuelIdeho <ohis98@gmail.com>
2025-05-27 19:28:33 -0400

    backend: completed RESTful endpoints in appointment controller

--- backend/src/main/java/com/example/backend/controller/AppointmentController.java
+++ backend/src/main/java/com/example/backend/controller/AppointmentController.java
@@ -1,15 +1,80 @@
 package com.example.backend.controller;
 
-//import org.springframework.http.ResponseEntity;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import com.example.backend.model.Appointment;
+import com.example.backend.service.AppointmentService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
-@RequestMapping("/api/")
+@RequestMapping("/api/v1")
 public class AppointmentController {
+
+    private final AppointmentService appointmentService;
 
     @GetMapping("/hello")
     public String helloWorld(){
         return "Hello world";
     }
+
+    @Autowired
+    public AppointmentController(AppointmentService appointmentService){
+        this.appointmentService = appointmentService;
+    }
+
+    @PostMapping("/appoint")
+    public ResponseEntity<Appointment> saveAppointment(@RequestBody Appointment appointment){
+        try{
+            Appointment newAppointment = appointmentService.saveAppointment(appointment);
+            return ResponseEntity.ok(newAppointment);
+        }catch(Exception e){
+             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        
+    }
+
+    @GetMapping("/appointments")
+    public List<Appointment> getAllAppointments(){
+        return appointmentService.getAllAppointments();
+    }
+
+    @GetMapping("/appointments/{id}")
+    public ResponseEntity<?> getAppointmentById(@PathVariable Long id){
+        try{
+            Optional<Appointment> appointment = appointmentService.getAppointmentById(id);
+            return ResponseEntity.ok(appointment);
+
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PutMapping("/appointments/{id}")
+    public ResponseEntity<?> updateAppointment(@PathVariable Long id, @RequestBody Appointment appointment){
+        try{
+            Appointment updatedAppointment = appointmentService.updateAppointment(id, appointment);
+        return ResponseEntity.ok(updatedAppointment);
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+    }
+
+    @DeleteMapping("/appointments/{id}")
+    public ResponseEntity<?> deleteAppointment(@PathVariable Long id){
+        try{
+            appointmentService.deleteAppointment(id);
+            return ResponseEntity.ok("Product deleted successfully");
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    
 }
