a0d682d145edec2d9dc161c1907ba89d6ddf9dea
EmmanuelIdeho <ohis98@gmail.com>
2025-05-26 20:24:58 -0400

    backend: set up Appointment repo interface

--- backend/src/main/java/com/example/backend/respository/AppointmentRepository.java
+++ backend/src/main/java/com/example/backend/respository/AppointmentRepository.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Repository;
 
 import com.example.backend.model.Appointment;
 
+/*
+ * Repository interface to handle database operations for the Appointment entity.
+ */
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
     
