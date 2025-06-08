b70302613ac10a185940f8945768ba69e875c4c9
EmmanuelIdeho <ohis98@gmail.com>
2025-06-08 15:39:03 -0400

    frontend: Implementd a loading spinner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,27 @@
 //import { useState } from 'react'
-import BookingForm from './components/BookingForm'
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Navbar from './components/Navbar'
-import AppointmentList from './components/AppointmentList';
 import './App.css'
-import Main from './components/Main';
-import NotFound from './components/NotFound';
+import React, { Suspense } from 'react';
+import LoadingSpinner from './components/LoadingSpinner';
+
 //import { Typography } from '@mui/material';
 //import axios from 'axios'
 
 
 function App() {
+  const Main = React.lazy(() => import('./components/Main'));
+  const BookingForm = React.lazy(() => import('./components/BookingForm'));
+  const AppointmentList = React.lazy(() => import('./components/AppointmentList'));
+  const NotFound = React.lazy(() => import('./components/NotFound'));
+
 
   return (
+
     <>
     <Router>
       <Navbar />
+      <Suspense fallback={<LoadingSpinner />}>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/main" element={<Main />} />
@@ -24,6 +30,7 @@ function App() {
         {/* Catch-all 404 route */}
         <Route path="*" element={<NotFound />} />
       </Routes>
+       </Suspense>
     </Router>
 
     
