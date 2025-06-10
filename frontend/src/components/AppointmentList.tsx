09881722ad12967089257f5c1a22bd1f488843a1
EmmanuelIdeho <ohis98@gmail.com>
2025-06-10 13:26:54 -0400

    frontend: changed appointlist list to a grid pattern:

--- frontend/src/components/AppointmentList.tsx
+++ frontend/src/components/AppointmentList.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import AppointmentCard from "./AppointmentCard";
 import { Appointment } from "../types/Appointment";
 import axios from "axios";
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 function AppointmentList() {
 
@@ -14,16 +14,24 @@ function AppointmentList() {
     .catch(err => console.log("Failed to fetch appointments", err));
   }, []);
     return (
-      <>
+     <>
       <Typography variant="h4" textAlign="center" mt={4} mb={2}>
         Upcoming Appointments
       </Typography>
-      <Box alignItems="left">
-        {appointments.length === 0 ? (<Typography>No appointments found</Typography>) : 
-        (appointments.map(app => (<AppointmentCard key={app.id} appointment={app}/>)))
-        }
+      <Box sx={{ flexGrow: 1, p: 2 }}>
+        {appointments.length === 0 ? (
+          <Typography textAlign="center">No appointments found</Typography>
+        ) : (
+          <Grid container spacing={3}>
+            {appointments.map(app => (
+              <Grid items xs={12} sm={6} md={4} lg={3} key={app.id}>
+                <AppointmentCard appointment={app} />
+              </Grid>
+            ))}
+          </Grid>
+        )}
       </Box>
-      </>
+    </>
       
     )
   }
