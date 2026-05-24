03f14a8c03af4ed8b983f08dff9bf4b9d96d4583
EmmanuelIdeho <ohis98@gmail.com>
2026-05-24 17:53:22 -0400

    test: verify form submission calls axios.post with correct appointment data

--- frontend/src/pages/BookingForm.test.tsx
+++ frontend/src/pages/BookingForm.test.tsx
@@ -2,7 +2,11 @@ import { render, screen } from '@testing-library/react'
 import BookingForm from './BookingForm'
 import userEvent from '@testing-library/user-event'
 import { fireEvent } from '@testing-library/react'
-// import your component here
+import axios from 'axios'
+import { vi } from 'vitest'
+
+vi.mock('axios')
+window.alert = vi.fn()
 
 describe('BookingForm', () => {
 it('renders all form fields and a submit button', () => {
@@ -52,7 +56,7 @@ it('the submit button is disabled when required fields are empty, and enabled on
 
     // Act: fill in all required fields
     await interact.type(name, "John Doe")
-    await interact.type(phone, "6477727132")
+    await interact.type(phone, "1111111111")
     await interact.type(email, "johndoe@gmail.com")
     fireEvent.change(date, {target: {value: "2026-03-03"}})
     fireEvent.change(time, { target: { value: "09:30" } })
@@ -60,6 +64,54 @@ it('the submit button is disabled when required fields are empty, and enabled on
     // Assert: button is now enabled
     expect(button).toBeEnabled()
 
+
+})
+
+it('submits the form and calls axios.post with the correct data', async() => {
+    // Arrange: Required fields
+    const interact = userEvent.setup() 
+    render(<BookingForm />)
+
+    const mockedPost = vi.mocked(axios.post)
+    mockedPost.mockResolvedValueOnce({
+    data: { patientName: 'John Doe', 
+            phoneNumber: '1111111111',
+            email: 'johndoe@gmail.com',
+            date: '2026-03-03',
+            time: '09:30'
+    }
+    })
+
+    const name = screen.getByLabelText(/your name/i)
+    const phone = screen.getByLabelText(/phone number/i)
+    const email = screen.getByLabelText(/your email/i)
+    const date = screen.getByLabelText(/date/i, { selector: 'input[type="date"]' })
+    const time = screen.getByLabelText(/time/i, { selector: 'input[type="time"]' })
+    const button = screen.getByRole("button", { name: /book appointment/i })
+
+   
+
+    // Act:
+    await interact.type(name, "John Doe")
+    await interact.type(phone, "1111111111")
+    await interact.type(email, "johndoe@gmail.com")
+    fireEvent.change(date, {target: {value: "2026-03-03"}})
+    fireEvent.change(time, { target: { value: "09:30" } })
+    await interact.click(button)
+   
+
+    // Assert:
+    expect(mockedPost).toHaveBeenCalledTimes(1)
+    expect(mockedPost).toHaveBeenCalledWith(
+  "undefined/api/v1/appoint",
+  {
+    patientName: "John Doe",
+    phoneNumber: "1111111111",
+    email: "johndoe@gmail.com",
+    date: "2026-03-03",
+    time: "09:30",
+  }
+)
 
 })
 
